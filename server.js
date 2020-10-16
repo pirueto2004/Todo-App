0ab366849b4570c40c8a8c7b453c3bd86b40a963
pirueto2004 <juliobell2014@gmail.com>
2020-10-15 20:49:45 -0400

    Updated mongoDB database connection

--- server.js
+++ server.js
@@ -1,64 +1,72 @@
 //Require the express module
-const express = require('express')
-require("dotenv").config();
+const express = require("express")
+require("dotenv").config()
 
 //Require MongoDB
-const mongodb = require('mongodb')
+const mongodb = require("mongodb")
 
 //Require sanitize-html
-const sanitizeHTML = require('sanitize-html')
+const sanitizeHTML = require("sanitize-html")
 
 //Create our express server
 const app = express()
 
-//Environment variable for mongoDB database password
-const mongodbPassword = process.env.MONGOPASSWORD
-const authenticatedUser = process.env.USER
+//Environment variables for mongoDB database
+const port = process.env.PORT,
+  database_name = process.env.DATABASE_NAME,
+  mongodbPassword = process.env.MONGOPASSWORD,
+  MONGODB_URI = process.env.CONNECTIONSTRING,
+  mongoURI = `mongodb://localhost:27017/${database_name}`,
+  authenticatedUser = process.env.USER
 
-
-//Store Database in a variable
-const DATABASE_NAME         = 'myTodoApp'
-
+//Define db variable
 let db
 
+//Environment variable for mongoDB database password
+// const mongodbPassword = process.env.MONGOPASSWORD
+// const authenticatedUser = process.env.USER
+
+//Store Database in a variable
+// const DATABASE_NAME = "myTodoApp"
+
 //Port for the server to listen
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000
 
 //Make the content in public folder available for the root of server
-app.use(express.static('public'))
-
-//Connection string
-const connectionString = `mongodb+srv://todoAppUser:${mongodbPassword}@jbcluster-v5kqr.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
-
-//if there's a shell environment variable named MONGODB_URI (deployed), use it; otherwise, connect to localhost
-const dbUrl = process.env.MONGODB_URI || connectionString;
-    
+app.use(express.static("public"))
 
 //Connecting our app to mongoDB Atlas
-mongodb.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-    //Select our MongoDB database
-    db = client.db()
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-    //Once MongoDB establishes a connection, server starts listening on port PORT
-    app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`))
+//Connection string
+// const connectionString = `mongodb+srv://todoAppUser:${mongodbPassword}@jbcluster-v5kqr.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
+
+//if there's a shell environment variable named MONGODB_URI (deployed), use it; otherwise, connect to localhost
+// const dbUrl = process.env.MONGODB_URI || connectionString
+
+//Connecting our app to mongoDB Atlas
+mongodb.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  //Select our MongoDB database
+  db = client.db()
+
+  //Once MongoDB establishes a connection, server starts listening on port PORT
+  app.listen(port, () => console.log(`Server listening on PORT ${port}`))
 })
-
-
 
 //Always add the following boilerplate code to your app for enabling the asyncronous requests to be accessible from the req.body object
 app.use(express.json())
 
 //Always add the following boilerplate code to your app for enabling the user request to be accessible from the req.body object
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 const passwordProtected = (req, res, next) => {
-    res.set('WWW-Authenticate', 'Basic realm="Simple To Do App"')
-    // console.log(req.headers.authorization)
-    if (req.headers.authorization == `${authenticatedUser}`) {
-        next()
-    } else {
-        res.status(401).send("Authentication required!")
-    }
+  res.set("WWW-Authenticate", 'Basic realm="Simple To Do App"')
+  // console.log(req.headers.authorization)
+  if (req.headers.authorization == `${authenticatedUser}`) {
+    next()
+  } else {
+    res.status(401).send("Authentication required!")
+  }
 }
 
 //This tells express to use our authentication function on all routes
@@ -66,11 +74,13 @@ const passwordProtected = (req, res, next) => {
 
 //Process incoming GET requests
 app.get("/", (req, res) => {
-    //Talk to the database and read all documents in the collection and convert it into a JS array
-    db.collection('items').find().toArray( (err, items) => {
-        // console.log(items)
-        //Respond to requests
-        res.send(`<!DOCTYPE html>
+  //Talk to the database and read all documents in the collection and convert it into a JS array
+  db.collection("items")
+    .find()
+    .toArray((err, items) => {
+      // console.log(items)
+      //Respond to requests
+      res.send(`<!DOCTYPE html>
         <html>
         <head>
         <meta charset="UTF-8">
@@ -115,41 +125,39 @@ app.get("/", (req, res) => {
 //         // console.log(req.body.item)
 //         // res.send("Thanks for submitting the form.")
 //     })
-    
+
 // })
 
 //Process POST request for creating item asynchronously
 app.post("/create-item", passwordProtected, (req, res) => {
-    //Sanitize or clean up text before inserting it in database
-    const safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}} )
-    //Create a new document in the collection
-    db.collection('items').insertOne({text: safeText}, (err, info) => {
-        //Send back the JS object that represents the new mongodb document we just created
-        res.json(info.ops[0])
-        // res.send("Success")
-        // console.log(req.body.item)
-        // res.send("Thanks for submitting the form.")
-    })
-    
+  //Sanitize or clean up text before inserting it in database
+  const safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: {} })
+  //Create a new document in the collection
+  db.collection("items").insertOne({ text: safeText }, (err, info) => {
+    //Send back the JS object that represents the new mongodb document we just created
+    res.json(info.ops[0])
+    // res.send("Success")
+    // console.log(req.body.item)
+    // res.send("Thanks for submitting the form.")
+  })
 })
 
 //Process POST request for updating item
 app.post("/update-item", passwordProtected, (req, res) => {
-    // console.log(req.body.text)
-    // res.send("Success")
-    //Sanitize or clean up text before inserting it in database
-    const safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}} )
-    db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: safeText}}, () => {
-        res.send("Success")
-    })
+  // console.log(req.body.text)
+  // res.send("Success")
+  //Sanitize or clean up text before inserting it in database
+  const safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: {} })
+  db.collection("items").findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: safeText } }, () => {
+    res.send("Success")
+  })
 })
 
 //Process POST request for deleting item
 app.post("/delete-item", passwordProtected, (req, res) => {
-    // console.log(req.body.text)
-    // res.send("Success")
-    db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, () => {
-        res.send("Success")
-    })
+  // console.log(req.body.text)
+  // res.send("Success")
+  db.collection("items").deleteOne({ _id: new mongodb.ObjectId(req.body.id) }, () => {
+    res.send("Success")
+  })
 })
-
